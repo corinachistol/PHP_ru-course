44afaf9c4128eb3e1e7ce29ddf9816b9b7fc4e7d
corina <chistolcorina19@gmail.com>
2024-03-22 16:02:32 +0200

    some changes

--- booking/src/contacts.php
+++ booking/src/contacts.php
@@ -1,4 +1,3 @@
-<h3 style="text-align:center">Contact us through the following methods:</h3>
 
 <?php 
     $city = "Chisinau";
@@ -6,33 +5,18 @@
     $phone = 69458741;
     $email = "eshop@example.com";
     $working_hours = "08:00-17:00";
+    
+    ?>
 
-?>
-
-
-<ul style = "list-style-type:none">
-    <li>
-        We are located in <?php print ($city) ?> city. 
-    </li><br>
-    <li>
-        Street: <?php print ($street) ?>.
-    </li><br>
-    <li>
-        <span>&#9990;</span> Call us <a href="#"> <?php print ($phone) ?></a> between our working hours: <?php print($working_hours) ?>. 
-    </li><br>
-    <li>
-        <span>&#9997;</span> Email us your problem: <a href="#"> <?php print ($email) ?></a>.
-    </li><br>
-    <li>
-        Find us on map!<br>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21773.75600550357!2d28.8727325!3d46.986843799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c9794f8e48f42f%3A0x2cf814bcaef210a1!2sCentrul%20de%20Excelen%C8%9B%C4%83%20%C3%AEn%20Informatic%C4%83%20%C8%99i%20Tehnologii%20Informa%C8%9Bionale!5e0!3m2!1sen!2s!4v1711112481925!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </li>
-</ul>
-
-
-
-
-
+<h1 style="text-align:center">Contact the best Travel Agency in town!!!</h1>
+<h3>Our Contacts</h3>
+    <hr>
+    <p><b>Address:</b> We are located in <?= $city?> </p>
+    <p><b>Street:</b> <?= $street ?>.</p>
+    <p><b><span>&#9990;</span> Phone us:</b><a href="#"><?= $phone ?></a> between our working hours: <?=$working_hours?>. </p>
+    <p><b><span>&#9997;</span> Email us</b> your problem: <a href="#"> <?=$email ?></a>.</p>
+    <p>Find us on map!<br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21773.75600550357!2d28.8727325!3d46.986843799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c9794f8e48f42f%3A0x2cf814bcaef210a1!2sCentrul%20de%20Excelen%C8%9B%C4%83%20%C3%AEn%20Informatic%C4%83%20%C8%99i%20Tehnologii%20Informa%C8%9Bionale!5e0!3m2!1sen!2s!4v1711112481925!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
 
 <!-- 
 
