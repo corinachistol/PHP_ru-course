1bea2974c461c61f95070b1b63d026cbba26a2c1
corina <chistolcorina19@gmail.com>
2024-03-22 15:33:54 +0200

    contacts.php created

---
+++ booking/src/contacts.php
@@ -0,0 +1,46 @@
+<h3 style="text-align:center">Contact us through the following methods:</h3>
+
+<?php 
+    $city = "Chisinau";
+    $street = "Sarmisegetusa St 48";
+    $phone = 69458741;
+    $email = "eshop@example.com";
+    $working_hours = "08:00-17:00";
+
+?>
+
+
+<ul style = "list-style-type:none">
+    <li>
+        We are located in <?php print ($city) ?> city. 
+    </li><br>
+    <li>
+        Street: <?php print ($street) ?>.
+    </li><br>
+    <li>
+        <span>&#9990;</span> Call us <?php print ($phone) ?> between our working hours: <?php print($working_hours) ?>. 
+    </li><br>
+    <li>
+        <span>&#9997;</span> Email us your problem: <?php print ($email) ?>.
+    </li><br>
+    <li>
+        Find us on map!<br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21773.75600550357!2d28.8727325!3d46.986843799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c9794f8e48f42f%3A0x2cf814bcaef210a1!2sCentrul%20de%20Excelen%C8%9B%C4%83%20%C3%AEn%20Informatic%C4%83%20%C8%99i%20Tehnologii%20Informa%C8%9Bionale!5e0!3m2!1sen!2s!4v1711112481925!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </li>
+</ul>
+
+
+
+
+
+
+<!-- 
+
+    HW1: create the page contacts.php
+        declare 5-6 varialbel with contact data:
+            - city, street, phone, email, geo coords, ...
+
+            - print the contact data -formatiing it using only HTML
+
+
+ -->
---
+++ booking/src/index.php
@@ -0,0 +1,21 @@
+<p>Welcome to our HOTEL Booking</p>
+<?php
+    $days_till_opening = 28;  //snake _case_name
+    $bonusPlaces = 10;          //camelCase
+?>
+<p>Days till opening <?php print($days_till_opening) ?></p>
+<p>Bonus places left <?php print($bonusPlaces) ?></p>
+<p>Don't hesitate to early book us!</p>
+
+<a href="./contacts.php">Contact Us</a>
+
+<!-- 
+
+    HW1: create the page contacts.php
+        declare 5-6 varialbel with contact data:
+            - city, street, phone, email, geo coords, ...
+
+            - print the contact data -formatiing it using only HTML
+
+
+ -->
