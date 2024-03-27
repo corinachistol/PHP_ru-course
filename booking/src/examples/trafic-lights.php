199b1e8b08a63b27a64b03cd2c304fbce9d6a0fc
corina <chistolcorina19@gmail.com>
2024-03-27 14:28:23 +0200

    traffic-lights HW

--- booking/src/examples/trafic-lights.php
+++ booking/src/examples/trafic-lights.php
@@ -11,3 +11,87 @@
 a-href-switch-traffic-lights.ru.md
 
 -->
+
+<!-- LOGIC -->
+<?
+    
+    $color = $_GET['color'] ?? "gray";
+
+    switch($color){
+        case "red":
+            $active_color = "red"; break;
+        case "orange":
+            $active_color = "orange"; break;
+        case "green":
+            $active_color = "green"; break;
+        default:
+            $active_color = "gray"; break;
+    }
+
+    
+?>
+
+<!-- Template -->
+<style>
+    body{
+        background-color: #222;
+        color:white;
+    }
+    section{
+       margin-top: 50px;
+
+    }
+    h3{
+        text-align: center;
+
+    }
+    .traffic_lights{
+        transform: rotate(90deg) translateX(70px) ;
+
+    }
+    .traffic_lights a {
+        display: inline-block;
+        background-color:gray; 
+        width:50px;
+        height:50px;
+        border-radius: 50%;
+        opacity: 30%;
+        margin-left: 5px;
+    }
+    .traffic_lights a:active{
+        opacity: 100%;
+    }
+
+    #red{
+        background-color: red;
+    }
+
+    #orange{
+        background-color: orange;
+    }
+
+    #green{
+        background-color: green;
+    }
+</style>
+<section>
+    <h3>Traffic Lights</title>
+
+    <div class="traffic_lights">
+        <a 
+            id="red" 
+            href="/examples/trafic-lights.php?color=red" 
+            class=<?$active_color?>>
+        </a>
+        <a 
+            id="orange" 
+            href="/examples/trafic-lights.php?color=orange" 
+            class=<?$active_color?>>
+        </a>
+        <a 
+            id="green" 
+            href="/examples/trafic-lights.php?color=green"
+            class=<?$active_color?>>
+        </a>
+    </div>
+</section>
