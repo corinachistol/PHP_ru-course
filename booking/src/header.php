076d41242e975e1f313b582d71d7431d7487c90e
corina <chistolcorina19@gmail.com>
2024-04-02 16:28:26 +0300

    carousel bootstrap

--- booking/src/header.php
+++ booking/src/header.php
@@ -1,18 +1,38 @@
 <header>
-    <nav>
-        <a href="/">XEBOOKING</a>
-        <a 
-            href="/?page=support" 
-            <? if ($page == 'support'){?>class="active"<?}?>
-        >support</a>
-        
-        <a 
-            href="/?page=contacts"
-             <? if ($page == 'contacts'){?>class="active"<?}?>
-        >contacts</a>
-        <a 
-            href="/?page=tours"
-             <? if ($page == 'tours'){?>class="active"<?}?>
-        >[Tour Catalog]</a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <!-- brand name -->
+            <a class="navbar-brand" href="/">XEBOOKING</a>
+            <!-- toggle button , responsiveness -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- link navbar -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a 
+                        href="?page=support"  
+                        class="nav-link <?= ($page == 'support') ? 'active' : '' ?> ">Support</a>
+                </li>
+
+                <li class="nav-item">
+                    <a 
+                        href="?page=contacts" 
+                        class="nav-link <?= ($page == 'contacts') ? 'active' : '' ?>">Contacts</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="?page=tours" class="nav-link <?= ($page == 'tours') ? 'active' : '' ?>">Tour Catalog</a>
+                </li>
+            </ul>
+            <!-- search form -->
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
     </nav>
+
 </header>
--- booking/src/tours.php
+++ booking/src/tours.php
@@ -4,7 +4,7 @@
 <!-- TEMPLATE -->
 <section>
     <h1>Tour Catalog</h1>
-    <ol>
+    <!-- <ol>
         <?  for ($i = 0; $i < count($tours); $i++) { ?>
             <li>
                <h2><?= $tours[$i]['name']?></h2>
@@ -29,4 +29,58 @@
             </li>
         <? } ?>
     </ol>
+   -->
+
+    <div class="container-fluid">
+        <? for($i = 0; $i < count($tours); $i++) { ?>
+
+            <div class="card" style="width: 18rem;">
+                <!-- carousel -->
+                <div id="carouselIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                         <? foreach( $tours[$i]['image'] as $values ) {?>
+                            <?php $isActive = true; ?>
+                            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" 
+                            class="<?= $isActive ? 'active' : '' ?>" aria-current="true" aria-label="Slide <?=$values[$i]?>"></button>
+                        <?}?>
+                    </div>
+                
+                    <div class="carousel-inner">
+
+                        <?php $isActive = true; ?>
+                        <? foreach( $tours[$i]['image'] as $values ) { ?>
+                            <div class="carousel-item <?= $isActive ? 'active' : '' ?>">
+                                <img 
+                                    src="<?= $values ?>" 
+                                    class="d-block w-100" 
+                                    alt="<?= $tours[$i]['name'] ?>"
+                                >
+                            </div>
+                            <?php $isActive = false; ?>
+                        <? } ?>
+
+                    </div>
+
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+                <!-- card body -->
+                <div class="card-body">
+                    <h5 class="card-title"><?= $tours[$i]['name'] ?></h5>
+                    <p class="card-text"><?= $tours[$i]['description'] ?></p>
+                    <a href="#" class="btn btn-primary ">See details</a>
+                </div>
+            </div>
+            
+        <?}?>
+    </div>
+
 </section>
