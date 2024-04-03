9ecf95930d85595f7abe48b31494d0d033f3aed5
corina <chistolcorina19@gmail.com>
2024-04-03 13:21:29 +0300

    hw json file

--- booking/src/examples/fs/writer.php
+++ booking/src/examples/fs/writer.php
@@ -2,17 +2,37 @@
     // ?name=John Doe&email=jd@example.com&age=30
 
         //HW1:using array functions copy only these 3 cells from the GEt array - >client array
-    
-        $name = $_GET['name'];
-        $email = $_GET['email'];
-        $age = $_GET['age'];   //HW2: convert age to integer
-                                // Hw3: get active (true,false) - boolean
+        $client= [];
 
-        $client = [  
-            'name' => $name,
-            'email'=> $email,
-            'age' => $age,
-        ];
+        if(array_key_exists('name',$_GET) &&
+           array_key_exists('email', $_GET) &&
+           array_key_exists('age', $_GET) ){
+
+            $name = $_GET['name'];
+            $email = $_GET['email'];
+            $age = (int)$_GET['age'];
+
+            // array_push($client, $name, $email, $age); // asa se introduct doar valorile,
+            array_push($client, [
+                'name' => $name,
+                'email'=> $email,
+                'age' => $age,
+            ]);
+          
+        }
+
+        if(array_key_exists('active', $_GET)){
+            $active = (bool)$_GET['active'];
+            $client[0]['active'] = $active ;
+             
+        }
+          var_dump($client);
+
+        //HW2: convert age to integer
+        // Hw3: get active (true,false) - boolean
+
+
+        
 
 
         //save to file
