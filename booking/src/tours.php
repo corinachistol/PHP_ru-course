8bb2ecab31584df1c51dd4b52c0c1fd4a328310a
corina <chistolcorina19@gmail.com>
2024-04-05 14:53:33 +0300

    carousel indicators error

--- booking/src/tours.php
+++ booking/src/tours.php
@@ -3,84 +3,78 @@
 
 <!-- TEMPLATE -->
 <section>
-    <h1>Tour Catalog</h1>
-    <!-- <ol>
-        <?  for ($i = 0; $i < count($tours); $i++) { ?>
-            <li>
-               <h2><?= $tours[$i]['name']?></h2>
-               <p><?= $tours[$i]['description']?></p>
+    <h1 class="text-center py-3">Tour Catalog</h1>
+        
+   <section class="container shadow">
+        <div class="row">
+            <? for ($i = 0; $i < count($tours); $i++) { ?>
+                <div class="col-md-6">
 
-               <div>
-                    <? foreach( $tours[$i]['image'] as $values ) { ?>
-                        <img 
-                            src="<?=$values?>" 
-                            alt="<?= $tours[$i]['name'] ?>"
-                            width="300">
-                    <?}?>
-               </div>
+                    <div class="card my-2 mx-2" >
 
-               <div>
-                    <?= $tours[$i]['price']['amount'] ?>
-                    <?= $tours[$i]['price']['currency'] ?>
-                    <span>
-                        <?= $tours[$i]['discount']['amount'] ?><?= $tours[$i]['discount']['percentage'] ?>
-                    </span>
-               </div>
-            </li>
-        <? } ?>
-    </ol>
-   -->
+                        <div id="carouselExampleIndicators" class="carousel slide">
+                            <div class="carousel-indicators">
+                                
+                                 <? foreach ($tours[$i]['image'] as $img) { ?>
+                                        
+                                        <?php $isActive = true; ?>
+                                        <button 
+                                            type="button" 
+                                            data-bs-target="#carouselExampleIndicators" 
+                                            data-bs-slide-to="<?= $img[$i] ?>"  
+                                            class="<?= $isActive ? 'active' : '' ?>" 
+                                            aria-current="<?= $isActive ? 'true' : '' ?>" 
+                                            aria-label="Slide <?= $img[$i] ?>"></button>
+                                <? } ?>
 
-    <div class="container-fluid">
-        <? for($i = 0; $i < count($tours); $i++) { ?>
-
-            <div class="card" style="width: 18rem;">
-                <!-- carousel -->
-                <div id="carouselIndicators" class="carousel slide">
-                    <div class="carousel-indicators">
-                         <? foreach( $tours[$i]['image'] as $values ) {?>
-                            <?php $isActive = true; ?>
-                            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" 
-                            class="<?= $isActive ? 'active' : '' ?>" aria-current="true" aria-label="Slide <?=$values[$i]?>"></button>
-                        <?}?>
-                    </div>
-                
-                    <div class="carousel-inner">
-
-                        <?php $isActive = true; ?>
-                        <? foreach( $tours[$i]['image'] as $values ) { ?>
-                            <div class="carousel-item <?= $isActive ? 'active' : '' ?>">
-                                <img 
-                                    src="<?= $values ?>" 
-                                    class="d-block w-100" 
-                                    alt="<?= $tours[$i]['name'] ?>"
-                                >
+                                <!-- <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
                             </div>
-                            <?php $isActive = false; ?>
-                        <? } ?>
+
+                            <div class="carousel-inner">
+
+                                <!-- <?php $isActive = true; ?> -->
+                                <? foreach ($tours[$i]['image'] as $img) { ?>
+                                    <div class="carousel-item <?= $isActive ? 'active' : '' ?>">
+                                        <img 
+                                            src="<?= $img ?>" 
+                                            class="d-block w-100" 
+                                            alt="<?= $tours[$i]['name'] ?>"
+                                        >
+                                    </div>
+                                    <!-- <?php $isActive = false; ?> -->
+                                  
+                                <? } ?>
+                            </div>
+
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+
+
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $tours[$i]['name'] ?></h5>
+                            <p class="card-text"><?= $tours[$i]['description']?></p>
+                            <a href="#" class="btn btn-primary">See details</a>
+                        </div>
 
                     </div>
 
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
+            <?}?>
+        </div>
+    </section>
 
-                <!-- card body -->
-                <div class="card-body">
-                    <h5 class="card-title"><?= $tours[$i]['name'] ?></h5>
-                    <p class="card-text"><?= $tours[$i]['description'] ?></p>
-                    <a href="#" class="btn btn-primary ">See details</a>
-                </div>
-            </div>
-            
-        <?}?>
-    </div>
+
+   
 
 </section>
+
+
